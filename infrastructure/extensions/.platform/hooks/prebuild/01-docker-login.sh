fa5b651f8cb4304d08c2204f0a6d370b1cb29146
CollinHva <Collin.poetoehena@hva.nl>
2025-03-03 16:53:01 +0100

    new docker login script

--- infrastructure/extensions/.platform/hooks/prebuild/01-docker-login.sh
+++ infrastructure/extensions/.platform/hooks/prebuild/01-docker-login.sh
@@ -4,6 +4,22 @@
 # For documentation see: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/platforms-linux-extend.hooks.html 
 
 echo "Logging into Docker Hub during prebuild from custom docker-login.sh script..."
+
+# Ensure that DOCKER_USERNAME and DOCKER_ACCESS_TOKEN are set before running docker login
+if [[ -z "$DOCKER_USERNAME" || -z "$DOCKER_ACCESS_TOKEN" ]]; then
+  echo "Error: DOCKER_USERNAME or DOCKER_ACCESS_TOKEN is not set. Exiting..."
+  exit 1
+fi
+
+# Perform the login
 # Use specific script with echo, since AWS EB uses a non-interactive terminal, and will otherwise error with:
 # Stderr:Error: Cannot perform an interactive login from a non TTY (interactive) device
 echo "$DOCKER_ACCESS_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Verify login success
+if [[ $? -eq 0 ]]; then
+  echo "Docker login successful!"
+else
+  echo "Docker login failed!"
+  exit 1
+fi
