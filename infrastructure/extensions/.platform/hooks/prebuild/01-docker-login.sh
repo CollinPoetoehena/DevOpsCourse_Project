de4c442e85bb0abc106c4c94b9c17e4c65129883
CollinHva <Collin.poetoehena@hva.nl>
2025-03-03 16:46:21 +0100

    test with new prebuild script

--- infrastructure/extensions/.platform/hooks/prebuild/01-docker-login.sh
+++ infrastructure/extensions/.platform/hooks/prebuild/01-docker-login.sh
@@ -4,4 +4,6 @@
 # For documentation see: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/platforms-linux-extend.hooks.html 
 
 echo "Logging into Docker Hub during prebuild from custom docker-login.sh script..."
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_ACCESS_TOKEN"
+# Use specific script with echo, since AWS EB uses a non-interactive terminal, and will otherwise error with:
+# Stderr:Error: Cannot perform an interactive login from a non TTY (interactive) device
+echo "$DOCKER_ACCESS_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
