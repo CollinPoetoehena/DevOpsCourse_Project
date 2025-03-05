26dfbd23f1ddc0bfc505f0dfa1161298a4a2338c
CollinHva <Collin.poetoehena@hva.nl>
2025-03-05 14:46:44 +0100

    frontend without .env allowed

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -2,10 +2,8 @@ const title = "Rent a Car";
 
 // Set URLs from the .env file (next.js has build in support for .env files: https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables)
 // Environment variables have to include the prefix NEXT_PUBLIC_ to be available for the browser, otherwise it shows undefined
-const baseUrl = process.env.NEXT_PUBLIC_FRONTEND_URL;
-const racUrl = process.env.NEXT_PUBLIC_BACKEND_FULL_URL;
-// const baseUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
-// const racUrl = process.env.BACKEND_FULL_URL || 'http://localhost:4001/api/v1';
+const baseUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
+const racUrl = process.env.BACKEND_FULL_URL || 'http://localhost:4001/api/v1';
 
 // Log variables for debugging purposes
 console.log("NEXT_PUBLIC_FRONTEND_URL:", baseUrl);
