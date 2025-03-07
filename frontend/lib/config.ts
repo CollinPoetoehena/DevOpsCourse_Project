67b683214beb1e327e1f625081abd0c81130001a
CollinHva <Collin.poetoehena@hva.nl>
2025-03-07 15:38:10 +0100

    refactor: change to using .env vars for cognito config

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -6,36 +6,25 @@ const baseUrl = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
 const racUrl = process.env.NEXT_PUBLIC_BACKEND_FULL_URL || 'http://localhost:4001/api/v1';
 
 // Cognito Configuration
-// const cognitoConfig = {
-//     region: process.env.NEXT_PUBLIC_AWS_REGION || 'us-east-1',
-//     userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || '',
-//     userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_APP_CLIENT_ID || '',
-//     oauth: {
-//         domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN || '',
-//         scope: ['openid', 'email', 'profile'],
-//         redirectSignIn: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_SIGN_IN || `${baseUrl}/auth/callback`,
-//         redirectSignOut: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_SIGN_OUT || `${baseUrl}/`,
-//         responseType: 'code', // Use 'code' for Authorization Code Grant
-//     }
-// };
-const cognitoClientId = "3q7iimb5obea8ik0bb1aiv2bao";
-const cognitoDomain = "https://rac-main-user-pool-domain.auth.eu-central-1.amazoncognito.com";
-const cognitoLogoutUri = "http://localhost:3000/auth/logout";
+const cognitoClientId = process.env.NEXT_PUBLIC_COGNITO_APP_CLIENT_ID;
+const cognitoDomain = process.env.NEXT_PUBLIC_COGNITO_DOMAIN;
+const cognitoLogoutUri = process.env.NEXT_PUBLIC_COGNITO_REDIRECT_SIGN_OUT;
 const cognitoAuthConfig = {
-  // Authority is the static url, but then with the region replaced and after the / the ID of the User pool
-  authority: "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_VVebgtNmw",
+  // Authority is the static url, but then with the region replaced and after the / the ID of the User pool, such as:
+  // https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_VVebgtNmw
+  authority: process.env.NEXT_PUBLIC_COGNITO_AUTHORITY,
   client_id: cognitoClientId,
   // Must be present in callback_urls specified, otherwise it errors with something like "redirect_mismatch" 
   // Uses specific page for the callback to handle the callback after the login/signup
-  redirect_uri: "http://localhost:3000/auth/callback",
+  redirect_uri: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_SIGN_IN,
   response_type: "code",
   scope: "phone openid email",
 };
-// TODO: change to using environment variables and remove first approach (commented out now) later as it did not work
 
 // Log variables for debugging purposes
 console.log("NEXT_PUBLIC_FRONTEND_URL:", baseUrl);
 console.log("NEXT_PUBLIC_BACKEND_FULL_URL:", racUrl);
+console.log("NEXT_PUBLIC_COGNITO_REDIRECT_SIGN_OUT:", cognitoLogoutUri);
 
 const config = {
     title,
