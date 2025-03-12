817cc160804868bc34391988f94d0702a42d9b59
CollinHva <Collin.poetoehena@hva.nl>
2025-03-12 08:57:53 +0100

    feat: automatically add users with different roles in the AWS user pool with Terraform

--- infrastructure/terraform/modules/cognito/main.tf
+++ infrastructure/terraform/modules/cognito/main.tf
@@ -4,6 +4,7 @@
 # AWS Cognito user pool: https://registry.terraform.io/providers/hashicorp/aws/5.88.0/docs/resources/cognito_user_pool
 resource "aws_cognito_user_pool" "main_user_pool" {
   name                     = var.cog_user_pool_name
+  # Configure email to be verified automatically
   auto_verified_attributes = ["email"]
 
   # Set username to case sensitive (this is the username used for sign-in and might be a bit confusing, since below
@@ -11,10 +12,12 @@ resource "aws_cognito_user_pool" "main_user_pool" {
   username_configuration {
     case_sensitive = true
   }
-  # Users can sign in using email, this basically says that the email can be used as username for sign-in
-  alias_attributes = ["email"]
+  # Users can sign in using aliases, such as email, this basically says that the email can be used as username for sign-in
+  # However, this is not used here to allow multiple users with the same email address (but different usernames). 
+  # The code is left here for potential future changes, since it is already tested with this option previously when it was still used 
+  # alias_attributes = ["email"]
 
-  // Require email for sign-up (username and password should already be included automatically)
+  # Require email for sign-up (username and password should already be included automatically)
   schema {
     attribute_data_type = "String"
     name               = "email"
@@ -62,8 +65,8 @@ resource "aws_cognito_user_pool_client" "main_user_pool_client" {
   ]
 }
 
-// User groups in the user pool (used for assigning roles in our application). 
-// No need to add custom AWS IAM roles, these are just used inside our application
+# User groups in the user pool (used for assigning roles in our application). 
+# No need to add custom AWS IAM roles, these are just used inside our application
 resource "aws_cognito_user_group" "cog_user_group_maintainer" {
   name         = var.cog_user_group_maintainer
   user_pool_id = aws_cognito_user_pool.main_user_pool.id
@@ -74,4 +77,69 @@ resource "aws_cognito_user_group" "cog_user_group_admin" {
   name         = var.cog_user_group_admin
   user_pool_id = aws_cognito_user_pool.main_user_pool.id
   description  = "Admin role in our application"
+}
+
+# Create initial users for development (this can be skipped for production environments or the password can be changed)
+# https://registry.terraform.io/providers/hashicorp/aws/5.88.0/docs/resources/cognito_user
+# This is a user with the maintainer role
+resource "aws_cognito_user" "user_maintainer" {
+  user_pool_id = aws_cognito_user_pool.main_user_pool.id
+  username     = "maintainer"
+
+  # Set attributes, including email (where the welcome message will be send to)
+  attributes = {
+    email          = "collin.poetoehena@student.uva.nl"
+    # Set email verified to true to skip manual verification step for this automatically created user
+    email_verified = true
+  }
+
+  # Set temporary password. In Development this can stay the same (fill in same password on first sign-in), 
+  # in production this can be changed on first sign-in or these users can be skipped completely for example 
+  temporary_password = "Dev123dev#"
+}
+# Add the user to the maintainer group, assigning a maintainer role: https://registry.terraform.io/providers/hashicorp/aws/5.88.0/docs/resources/cognito_user_in_group
+resource "aws_cognito_user_in_group" "add_user_maintainer" {
+  user_pool_id = aws_cognito_user_pool.main_user_pool.id
+  group_name   = aws_cognito_user_group.cog_user_group_maintainer.name
+  username     = aws_cognito_user.user_maintainer.username
+}
+
+# This is a user with the admin role
+resource "aws_cognito_user" "user_admin" {
+  user_pool_id = aws_cognito_user_pool.main_user_pool.id
+  username     = "admin"
+
+  # Set attributes, including email (where the welcome message will be send to)
+  attributes = {
+    email          = "collin.poetoehena@student.uva.nl"
+    # Set email verified to true to skip manual verification step for this automatically created user
+    email_verified = true
+  }
+
+  # Set temporary password. In Development this can stay the same (fill in same password on first sign-in), 
+  # in production this can be changed on first sign-in or these users can be skipped completely for example 
+  temporary_password = "Dev123dev#"
+}
+# Add the user to the admin group, assigning an admin role
+resource "aws_cognito_user_in_group" "add_user_admin" {
+  user_pool_id = aws_cognito_user_pool.main_user_pool.id
+  group_name   = aws_cognito_user_group.cog_user_group_admin.name
+  username     = aws_cognito_user.user_admin.username
+}
+
+# Add normal user (does not have to be added to a group, since no group means the role is normal user)
+resource "aws_cognito_user" "user_normal_user" {
+  user_pool_id = aws_cognito_user_pool.main_user_pool.id
+  username     = "user"
+
+  # Set attributes, including email (where the welcome message will be send to)
+  attributes = {
+    email          = "collin.poetoehena@student.uva.nl"
+    # Set email verified to true to skip manual verification step for this automatically created user
+    email_verified = true
+  }
+
+  # Set temporary password. In Development this can stay the same (fill in same password on first sign-in), 
+  # in production this can be changed on first sign-in or these users can be skipped completely for example 
+  temporary_password = "Dev123dev#"
 }
--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -1,4 +1,4 @@
-// Cloud provider: https://registry.terraform.io/providers/hashicorp/aws/5.88.0/docs
+# Cloud provider: https://registry.terraform.io/providers/hashicorp/aws/5.88.0/docs
 terraform {
   required_providers {
     aws = {
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "aws" { 
-  // Authentication: https://registry.terraform.io/providers/hashicorp/aws/5.88.0/docs#environment-variables
-  // Using the variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_REGION with export will automatically configure the provider
-  // See Terraform.md for how to set these variables
+  # Authentication: https://registry.terraform.io/providers/hashicorp/aws/5.88.0/docs#environment-variables
+  # Using the variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_REGION with export will automatically configure the provider
+  # See Terraform.md for how to set these variables
 }
