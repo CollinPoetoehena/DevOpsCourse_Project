b9eb48e232b723efa4239fbbe0d3b14c4ce96e8e
CollinHva <Collin.poetoehena@hva.nl>
2025-03-19 10:48:41 +0100

    fix: fix integration with prod environment and test environment

--- backend/test/teardownTests.js
+++ backend/test/teardownTests.js
@@ -1,6 +1,10 @@
 const { stopDatabase } = require('./mongo-memory-server');
 
 module.exports = async function globalTeardown() {
+    // Remove NODE_ENV env variable to avoid running in test environment in cloud deployments
+    process.env.NODE_ENV = '';
+
+    // Stop database
     await stopDatabase();
     console.log('MongoDB test database teardown completed');
 };
