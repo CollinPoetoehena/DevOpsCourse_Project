a4489efd410a1bcaaa1f47508b6cb90b50f5f9a5
CollinHva <Collin.poetoehena@hva.nl>
2025-03-19 11:06:55 +0100

    fix: tests now running from docker container

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    // Force Jest to search from the correct root
+    rootDir: ".",
+    // Explicitely state the path to the tests from the root of the backend folder
+    testMatch: ["**/test/**/*.test.js"],
     globalSetup: './test/setupTests.js',
     globalTeardown: './test/teardownTests.js',
     testEnvironment: 'node',
